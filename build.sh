c0c534a0f8d5f97b0feec2fb3bd7dc62d63eb1e1
Philipp Schuster <phip1611@gmail.com>
2022-07-25 11:09:47 +0200

    build.sh: build Hedron with nix toolchain + as release

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -x
 
 # nice "hack" which make the script work, even if not executed from "./"
 DIR=$(dirname "$(realpath "$0")")
-cd "$DIR" || exit
+cd "$DIR" || exit 1
 
 source .config.sh
 
@@ -18,36 +18,37 @@ function fn_main() {
 }
 
 function fn_build_hedron() {
-    OLD_PWD=$(pwd)
-    cd "./hedron" || exit
+    cd hedron || exit 1
+    # Use toolchain provided by Nix
+    # I don't use "nix-build nix/release.nix -A hedron" here because this
+    # way is faster because Nix needs to download less stuff.
+    nix-shell --run "
     mkdir -p build
-    cd build || exit
+    cd build || exit 1
     cmake -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release ..
-    make
-    cd "$OLD_PWD"
+    make -j 8"
+    cd ..
 }
 
 function fn_build_rust() {
-    OLD_PWD=$(pwd)
-    cd "./roottask" || exit
-    cargo build
-    cargo build --release
+    cd roottask || exit 1
+    cargo check
     cargo fmt -- --check
     cargo clippy
-    cd "$OLD_PWD"
+    cargo build --release
+    cd ..
 }
 
 # Strip debug symbols; much smaller ELF file. Accelerates QEMU startup by a second or so.
 # The strip step is only relevant for QEMU < 6.2. In QEMU 6.2, the startup of large Multiboot2
 # modules is accelerated by an order of magnitude.
 function fn_build_rust_strip() {
-    OLD_PWD=$(pwd)
-    cd "./roottask" || exit
+    cd roottask || exit 1
     cp "target/${COMPILER_TARGET}/debug/hmr" "target/${COMPILER_TARGET}/debug/hmr_stripped"
     strip "target/${COMPILER_TARGET}/debug/hmr_stripped"
     cp "target/${COMPILER_TARGET}/release/hmr" "target/${COMPILER_TARGET}/release/hmr_stripped"
     strip "target/${COMPILER_TARGET}/release/hmr_stripped"
-    cd "$OLD_PWD"
+    cd ..
 }
 
 # invoke main function
